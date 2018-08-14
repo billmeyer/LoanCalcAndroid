2c34ae313fdb49b1a3ec80c2b87a7020a3cb2474
Bill Meyer <stinkjuice@gmail.com>
2018-08-14 11:26:34 -0500

    Added command-line run examples as well.

--- run-runner.sh
+++ run-runner.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-runner.sh config --path runner.yml --apikey ${LOANCALC_ANDROID_APIKEY} $*
+APPAPK=./app/build/outputs/apk/debug/app-debug.apk
+TESTAPK=./app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
+# DEVICE=Google_Pixel_8_real_us
+
+#runner.sh config --path runner.yml --apikey ${LOANCALC_ANDROID_APIKEY} $*
+runner.sh espresso --test ${TESTAPK} --app ${APPAPK} --apikey ${LOANCALC_ANDROID_APIKEY} --datacenter US --e class io.billmeyer.loancalc.LoanCalcTest#calcLoanViaAPI --e class io.billmeyer.loancalc.LoanCalcTest#calcLoanViaUI $*

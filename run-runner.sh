fbfab0c3f47e08cb379e186fc6424248d3014b84
Bill Meyer <thebillmeyer@gmail.com>
2019-07-18 09:05:57 -0500

    Changed device ID

--- run-runner.sh
+++ run-runner.sh
@@ -3,7 +3,7 @@
 APPAPK=./app/build/outputs/apk/debug/app-debug.apk
 TESTAPK=./app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
 # DEVICE=Google_Pixel_8_real_us
-DEVICE=Samsung_Galaxy_S7_carrier_test_sjc
+DEVICE=Samsung_Galaxy_S6_POC133
 
 #runner.sh config --path runner.yml --apikey ${LOANCALC_ANDROID_APIKEY} $*
 runner.sh espresso --device ${DEVICE} --test ${TESTAPK} --app ${APPAPK} --apikey ${LOANCALC_ANDROID_APIKEY} --datacenter US --e class io.billmeyer.loancalc.LoanCalcTest#calcLoanViaAPI --e class io.billmeyer.loancalc.LoanCalcTest#calcLoanViaUI $*
